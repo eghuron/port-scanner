e91aeaf84430a967d3879ff0f9b03fa5182cccce
Érick <53627028+ghurone@users.noreply.github.com>
2020-04-29 18:00:59 -0300

    Deixando em função

--- port.py
+++ port.py
@@ -1,14 +1,15 @@
 import socket
 
-host = str(input('IP: '))
+def port_scanner(a):  # a = str 127.0.0.1
+    host = a
 
-print('Open Ports: ')
-for porta in range(1, 65536):
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.settimeout(1)
-    retorno = sock.connect_ex((host, porta))
+    print('Open Ports: ')
+    for porta in range(1, 65536):
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.settimeout(1)
+        retorno = sock.connect_ex((host, porta))
 
-    sock.close()
+        sock.close()
 
-    if retorno == 0:
-        print(porta)
+        if retorno == 0:
+            print(porta)
